bef5ed782215cf1b2d7fb576a7e259e21243bdb4
candycarcrash <164032784+candycarcrash@users.noreply.github.com>
2024-03-19 21:40:52 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# high-performance-computer
+
+Computer designed for high-performance computing tasks, supporting parallel processing.
+
+## Installation
+
+```bash
+git clone https://github.com/candycarcrash/high-performance-computer.git
+cd high-performance-computer
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+const graphql_query_builder = require('graphql-query-builder');
+const high_performance_computer = require('high-performance-computer');
+const debug = require('debug');
+const webpack = require('webpack');
+const solc = require('solc');
+const bluebird = require('bluebird');
+const node_sass = require('node-sass');
+const ganache_cli = require('ganache-cli');
+const passport = require('passport');
+const cors = require('cors');
+const moment = require('moment');
+const ipfs_http_client = require('ipfs-http-client');
+const truffle = require('truffle');
+const webpack_cli = require('webpack-cli');
+const mongoose = require('mongoose');
+const pg = require('pg');
+const react_dom = require('react-dom');
+const ethers = require('ethers');
+const validator = require('validator');
+
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
+const delay = async () => {
+  console.log('Start delay');
+  await setTimeoutPromise(1000);
+  console.log('End delay');
+};
+delay();
+
+// Verify a message signature
+const signerAddress = web3.eth.accounts.recover(messageHash, signature.signature);
+console.log('Signer address:', signerAddress);
+
+const url = require('url');
+const myUrl = new URL('https://example.com/about?category=nodejs');
+console.log('Protocol:', myUrl.protocol);
+console.log('Hostname:', myUrl.hostname);
+console.log('Pathname:', myUrl.pathname);
+
+const dns = require('dns');
+dns.lookup('example.com', (err, address, family) => {
+  console.log('address: %j family: IPv%s', address, family);
+});
